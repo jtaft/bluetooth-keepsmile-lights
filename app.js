42c4a0881384cef02d7b537ffd6dedcd366a0400
Jay Taft <ruforou@gmail.com>
2023-05-23 15:37:10 -0500

    Add ON OFF Functionality
    This change adds buttons to turn lights on and off and will
    automatically connect to devices that are saved.

--- app.js
+++ app.js
@@ -10,39 +10,16 @@ var options = {
   key: key,
   cert: cert
 };
-//    private static List<String> UUID_MODIFIERS = Arrays.asList("AFD0", "AFD1", "AFD2", "AFD3");
-//
-//    public static String DEVICE_NAME = "KS03~";
-//
-//    public static String SERVICE_READ = UUID(UUID_MODIFIERS.get(0));
-//    public static String SERVICE_WRITE = UUID(UUID_MODIFIERS.get(0));
-//    public static String SERVICE_NOTIFY = UUID(UUID_MODIFIERS.get(0));
-//
-//    public static String CHARACTERISTIC_READ = UUID(UUID_MODIFIERS.get(3));
-//    public static String CHARACTERISTIC_WRITE = UUID(UUID_MODIFIERS.get(1));
-//    public static String CHARACTERISTIC_NOTIFY = UUID(UUID_MODIFIERS.get(2));
 
-/*function hexToBytes(hex) {
-  let bytes = [];
-  for (let c = 0; c < hex.length; c += 2)
-      bytes.push(parseInt(hex.substr(c, 2), 16));
-  return bytes;
-}*/
-function hexToArrayBuffer(string) {
-    let bytes = [];
-    string.replace(/../g, function (pair) {
-        bytes.push(parseInt(pair, 16));
-    });
-    return new Uint8Array(bytes).buffer;
-}
+var SERVICE_UUID = "0000afd0-0000-1000-8000-00805f9b34fb";
 
-function typedArrayToBuffer(byteArray) {
-  var offset = 0;
-  var byteLength = byteArray.length;
-  var array = Uint8Array.from(byteArray);
-  var subarray = array.subarray(0, byteLength);
-  return array.buffer.slice(offset, byteLength + offset);
-}
+var CHARACTERISTIC_READ_UUID = "0000afd3-0000-1000-8000-00805f9b34fb";
+var CHARACTERISTIC_WRITE_UUID = "0000afd1-0000-1000-8000-00805f9b34fb";
+var CHARACTERISTIC_NOTIFY_UUID = "0000afd2-0000-1000-8000-00805f9b34fb";
+
+var LIGHTS_ON_STRING = "5BF000B5";
+var LIGHTS_OFF_STRING = "5B0F00B5";
+
 function byteToUint8Array(byteArray) {
   var uint8Array = new Uint8Array(byteArray.length);
   for(var i = 0; i < uint8Array.length; i++) {
@@ -52,35 +29,6 @@ function byteToUint8Array(byteArray) {
   return uint8Array;
 }
 
-var SERVICE_UUID = "0000afd0-0000-1000-8000-00805f9b34fb";
-var CHARACTERISTIC_READ_UUID = "0000afd3-0000-1000-8000-00805f9b34fb";
-var CHARACTERISTIC_WRITE_UUID = "0000afd1-0000-1000-8000-00805f9b34fb";
-var CHARACTERISTIC_NOTIFY_UUID = "0000afd2-0000-1000-8000-00805f9b34fb";
-//
-var LIGHTS_ON_STRING = "5BF000B5";
-var LIGHTS_OFF_STRING = "5B0F00B5";
-var encoder = new TextEncoder('utf-8');
-var LIGHTS_ON_COMMAND = encoder.encode(LIGHTS_ON_STRING);
-
-//typedArrayToBuffer(hexStr2Bytes(LIGHTS_ON_STRING));
-
-
-
-//Convert this Java code to JavaScript
-//public byte[] hexStr2Bytes(String str) {
-//        int length = str.length() / 2;
-//        char[] charArray = str.toCharArray();
-//        String[] strArr = new String[length];
-//        byte[] bArr = new byte[length];
-//        int i = 0;
-//        for (int i2 = 0; i2 < length; i2++) {
-//            strArr[i2] = "" + charArray[i] + charArray[i + 1];
-//            bArr[i2] = (byte) Integer.parseInt(strArr[i2], 16);
-//            i += 2;
-//        }
-//        return bArr;
-//    }
-//
 function hexStr2Bytes(str) {
   var length = str.length / 2;
   var charArray = str.split('');
@@ -95,121 +43,82 @@ function hexStr2Bytes(str) {
   return bArr;
 }
 
-
-
-//TODO: DATA: Lights off INT32 = 66051 Offset 0
-//
-//data = bytes(LIGHTS_ON_STRING, 'utf-8') # Data to write
-
-
-//Function to insert log into element with id="log"
 function insertLog(msg){
+  console.log(msg);
   document.getElementById("log").innerHTML += ("<p>" + msg + "</p>");
 }
-function onButtonClick() {
+function onButtonClick(command) {
   let filters = [];
+  document.getElementById("log").innerHTML = "";
 
-  let filterService = document.querySelector('#service').value;
-  if (filterService.startsWith('0x')) {
-    filterService = parseInt(filterService);
-  }
-  if (filterService) {
-    filters.push({services: [filterService]});
-  }
-
-  let filterName = document.querySelector('#name').value;
-  if (filterName) {
-    filters.push({name: filterName});
-  }
-
-  let filterNamePrefix = document.querySelector('#namePrefix').value;
-  if (filterNamePrefix) {
-    filters.push({namePrefix: filterNamePrefix});
-  }
-
-  let options = {optionalServices: [SERVICE_UUID]};
-  if (document.querySelector('#allDevices').checked) {
-    options.acceptAllDevices = true;
-  } else {
-    options.filters = filters;
-  }
-
-  console.log('Requesting Bluetooth Device...');
-  insertLog('Requesting Bluetooth Device...');
-  console.log('with ' + JSON.stringify(options));
-  insertLog('with ' + JSON.stringify(options));
-  if (navigator.bluetooth == undefined) {
-    console.log('navigator.bluetooth is undefined');
-    insertLog('navigator.bluetooth is undefined');
-    return;
-  }
-  navigator.bluetooth.requestDevice(options)
-  .then(device => {
-    console.log('> Name:             ' + device.name);
-    insertLog('> Name:             ' + device.name);
-    console.log('> Id:               ' + device.id);
-    insertLog('> Id:               ' + device.id);
-    return device.gatt.connect();
-  }).then(server => {
-    console.log('> Connected:        ' + server.connected);
-    insertLog('> Connected:        ' + server.connected);
-    console.log('Getting Services...');
-    insertLog('Getting Services...');
-    return server.getPrimaryService(SERVICE_UUID);
-  }).then(service => {
-    console.log('Getting Characteristics...');
-    insertLog('Getting Characteristics...');
-    return service.getCharacteristic(CHARACTERISTIC_WRITE_UUID);
-  }).then(characteristic => {
-    var command = byteToUint8Array(hexStr2Bytes(LIGHTS_ON_STRING));
-    //console.log(typeof(LIGHTS_ON_COMMAND));
-    //insertLog(typeof(LIGHTS_ON_COMMAND));
-    //console.log(typeof(LIGHTS_ON_COMMAND.buffer));
-    //insertLog(typeof(LIGHTS_ON_COMMAND.buffer));
-    //const encoder = new TextEncoder('utf-8');
-    characteristic.writeValueWithResponse(command).then((response) => {
-      console.log(response);
-      insertLog(response);
+  var device = navigator.bluetooth.getDevices().then(devices => {
+    // Filter on saved devices
+    for(let device of devices) {
+      if (device.name == 'KS03~791C47') {
+        insertLog('Device: ' + device.name);
+        insertLog('Device id: ' + device.id);
+        return device.gatt.connect();
+      }
+    }
+    // If no saved device found, prompt for new device
+    insertLog('No saved device found, prompting for new device...');
+    return new Promise(function (resolve, reject){
+      let options = {
+        filters: [
+          {namePrefix: 'KS03'},
+          {name: 'KS03~791C47'},
+          {services: [SERVICE_UUID]}
+        ],
+        acceptAllDevices: false,
+        optionalServices: [SERVICE_UUID]
+      };
+      console.log(navigator.bluetooth);
+      navigator.bluetooth.requestDevice(options)
+      .then(device => {
+        insertLog('> Name:             ' + device.name);
+        insertLog('> Id:               ' + device.id);
+        device.gatt.connect().then(resolve, reject);
+      }, reject);
     });
-    //for(let characteristic of characteristics) {
-    //  console.log('Characteristic: ' + characteristic.uuid);
-    //  insertLog('Characteristic: ' + characteristic.uuid);
-    //  console.log('Characteristic write: ' + characteristic.properties.write);
-    //  insertLog('Characteristic write: ' + characteristic.properties.write);
-    //  console.log('Characteristic read: ' + characteristic.properties.read);
-    //  insertLog('Characteristic read: ' + characteristic.properties.read);
-    //  console.log('Characteristic value: ' + characteristic.value);
-    //  insertLog('Characteristic value: ' + characteristic.value);
-    //}
-    //console.log('Characteristic: ' + characteristic.uuid);
-    //insertLog('Characteristic: ' + characteristic.uuid);
-    //console.log('Characteristic write: ' + characteristic.properties.write);
-    //insertLog('Characteristic write: ' + characteristic.properties.write);
-    //console.log('Characteristic read: ' + characteristic.properties.read);
-    //insertLog('Characteristic read: ' + characteristic.properties.read);
-    //characteristic.readValue().then(value => {
-    //  console.log('Characteristic offset: ' + (value.byteOffset));
-    //  insertLog('Characteristic offset: ' + (value.byteOffset));
-    //  console.log('Characteristic value: ' + value.getInt32(value.byteOffset));
-    //  insertLog('Characteristic value: ' + value.getInt32(value.byteOffset));
-    //});
+  }).then(function(server) {
+    var onError = function(error) {
+      insertLog('Internal! ' + error);
+      server.disconnect();
+    }
+    insertLog('> Connected:        ' + server.connected);
+    insertLog('Getting Services...');
+    server.getPrimaryService(SERVICE_UUID).then(service => {
+      insertLog('Getting Characteristics...');
+      return service.getCharacteristic(CHARACTERISTIC_WRITE_UUID);
+    }, onError).then(characteristic => {
+      var bytecommand = byteToUint8Array(hexStr2Bytes(command));
+      characteristic.writeValueWithoutResponse(bytecommand).then(() => {
+        if (command == LIGHTS_ON_STRING) {
+          insertLog('Lights ON');
+        } else if (command == LIGHTS_OFF_STRING) {
+          insertLog('Lights OFF');
+        }
+        server.disconnect();
+      }, onError);
+    }, onError);
   }).catch(error => {
-    console.log('Argh! ' + error);
     insertLog('Argh! ' + error);
   });
+}
+function lightsOnClick() {
+  onButtonClick(LIGHTS_ON_STRING);
+}
+function lightsOffClick() {
+  onButtonClick(LIGHTS_OFF_STRING);
 }
 var variables = `var CHARACTERISTIC_READ_UUID = "${CHARACTERISTIC_READ_UUID}";var CHARACTERISTIC_WRITE_UUID = "${CHARACTERISTIC_WRITE_UUID}";var CHARACTERISTIC_NOTIFY_UUID = "${CHARACTERISTIC_NOTIFY_UUID}"; var SERVICE_UUID = "${SERVICE_UUID}"; var LIGHTS_ON_STRING = "${LIGHTS_ON_STRING}";var LIGHTS_OFF_STRING = "${LIGHTS_OFF_STRING}";`;
 
 
 var formHtml = "<form onsubmit=\"return false\">" +
-  "<label for=\"allDevices\">All Devices</label>" +
-  "<input id=\"allDevices\" type=\"checkbox\">" +
-  "<input id=\"service\" type=\"text\" size=\"17\" list=\"services\" placeholder=\"Bluetooth Service\">" +
-  "<input id=\"name\" type=\"text\" size=\"17\" placeholder=\"Device Name\">" +
-  "<input id=\"namePrefix\" type=\"text\" size=\"17\" placeholder=\"Device Name Prefix\">" +
-  "<button onclick=\"onButtonClick()\">Get Bluetooth Device Info</button>" +
+  "<button style=\"width:50vw;height:5vh;margin-bottom:5vh;\" onclick=\"lightsOnClick()\">Lights ON</button>" +
+  "<button style=\"width:50vw;height:5vh;\" onclick=\"lightsOffClick()\">Lights OFF</button>" +
 "</form>" +
-"<script>" + hexStr2Bytes.toString() + typedArrayToBuffer.toString() + byteToUint8Array.toString() + variables + onButtonClick.toString() + "</script>" +
+"<script>" + hexStr2Bytes.toString() + byteToUint8Array.toString() + variables + lightsOffClick.toString() + lightsOnClick.toString() + onButtonClick.toString() + "</script>" +
 "<script>" + insertLog.toString() + "</script>";
 
 
